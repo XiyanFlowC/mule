4129ae9a0bc80bdc021432e0f7011c2dabc8b28e
XiyanFlowC <1197129380@qq.com>
2024-02-27 10:59:29 +0800

    变更前存档

--- mule/version.h
+++ mule/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H__
 #define VERSION_H__
 
-#define MULE_BUILD_TIME "2024/02/26-14:09:55"
+#define MULE_BUILD_TIME "2024/02/27-01:41:56"
 #define MULE_MAJOR_VERSION 1
-#define MULE_MINOR_VERSION 0
+#define MULE_MINOR_VERSION 5
 
 #endif
